c0ce2c8c29a5609774f23b572330baa23274c572
Headhunter1223 <44026630+Headhunter1223@users.noreply.github.com>
2021-05-03 17:35:18 +0300

    hw3

--- src/App.js
+++ src/App.js
@@ -1,25 +1,90 @@
 import logo from './logo.svg';
 import './App.css';
+import React from 'react'
+
+
 
 function App() {
   return (
     <div className="App">
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+        <MyTodoList />
       </header>
     </div>
   );
 }
 
 export default App;
+
+
+
+class MyTodoList extends React.Component {
+  state = {
+    tasks: [
+      {
+        id: 1234,
+        name: 'Name 1',
+        description: 'What needs to be done',
+        completed: 0
+      },
+      {
+        id: 1234,
+        name: 'Name 1',
+        description: 'What needs to be done',
+        completed: 0
+      },
+      {
+        id: 1234,
+        name: 'Name 1',
+        description: 'What needs to be done',
+        completed: 0
+      }
+    ]
+
+  }
+
+  MapTasks() {
+    console.log(typeof(this.state.tasks[0].id))
+    return this.state.tasks.map(t => (<Task id={t.id} name={t.name} description={t.description} completed = {t.completed}/>))
+  }
+
+  render() {
+    return (
+      <div>
+        {this.MapTasks()}
+      </div>
+    )
+  }
+}
+
+
+class Task extends React.Component {
+  constructor(props) {
+    super(props);
+
+    this.id = props.id;
+    this.name = props.name;
+    this.description = props.description;
+    this.completed = props.completed;
+  }
+
+  render() {
+    return (
+      <div>
+        <div>
+          {this.props.name}
+        </div>
+
+        <div>
+          {this.props.description}
+        </div>
+
+        <div>
+          {this.props.completed} 
+          <button />          
+        </div>
+
+      </div>
+    )
+  }
+}
